1065ca7c93dec57cb9f0fe1c97e1e8479dbf2033
Max Gilbert <mgilbert@aucklandmuseum.com>
2023-01-27 12:17:38 +1300

    config

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -11,8 +11,6 @@ export interface blogPost {
 
 export const contentfulClient = contentful.createClient({
   space: import.meta.env.CONTENTFUL_SPACE_ID,
-  accessToken: import.meta.env.DEV
-    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
-    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
-  host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
+  accessToken: import.meta.env.CONTENTFUL_PREVIEW_TOKEN,
+  host: "preview.contentful.com" 
 });
