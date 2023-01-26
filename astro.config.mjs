0a026fbe33096c217613a4710432f56266e3a4e6
Max Gilbert <mgilbert@aucklandmuseum.com>
2023-01-27 11:33:08 +1300

    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  adapter: netlify()
 });
