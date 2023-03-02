7ae9190f8321aaeb20959c80cfebd3406b8453fc
Max Gilbert <mgilbert@aucklandmuseum.com>
2023-03-02 14:50:23 +1300

    updates

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,4 +3,6 @@ interface ImportMetaEnv {
 	readonly CONTENTFUL_SPACE_ID: string;
 	readonly CONTENTFUL_DELIVERY_TOKEN: string;
 	readonly CONTENTFUL_PREVIEW_TOKEN: string;
+	readonly CONTENTFUL_SPACE_ID: string;
+	readonly CONTENTFUL_MANAGEMENT_API_ACCESS_TOKEN: string;
   }
