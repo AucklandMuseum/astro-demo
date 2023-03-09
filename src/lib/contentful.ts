216de68a0dbdf879d71c88e28937dd3f0e15c8e0
Max Gilbert <mgilbert@aucklandmuseum.com>
2023-03-09 17:42:43 +1300

    css

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -22,7 +22,6 @@ export function getPageBySlug(
 export function getPageByAltURL(
   altURL: string
 ): Promise<contentful.Entry<TypePage>> {
-  console.log(altURL)
   return client
     .getEntries({"locale":"en-NZ","content_type":"page", "fields.alternateUrls[in]":"/"+altURL, "include":10})
     .then((response: contentful.EntryCollection<TypePage>) => response.items[0])
