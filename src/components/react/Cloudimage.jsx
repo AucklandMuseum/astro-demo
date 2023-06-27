ccc28630695b4136195ea5457a04297be718a496
Max Gilbert <mgilbert@aucklandmuseum.com>
2023-06-27 12:40:31 +1200

    hero

--- src/components/react/Cloudimage.jsx
+++ src/components/react/Cloudimage.jsx
@@ -2,13 +2,21 @@ import Img, { CloudimageProvider } from 'react-cloudimage-responsive-plain';
 
 const cloudimageConfig = {
   token: 'ajrctguoxo',
-  lazyLoading: true
+  lazyLoading: true,
+  devicePixelRatioList: [1],
+  presets: {
+    xs: '(max-width: 639px)', 
+    sm: '(min-width: 640px)',
+    md: '(min-width: 768px)', 
+    lg: '(min-width: 1024px)', 
+    xl: '(min-width: 1280px)' 
+}
 };
 
 export default function MySidebar(props) {
   return (
     <CloudimageProvider config={cloudimageConfig}>
-      <Img src={props.src} params="org_if_sml=0&force_format=webp,jpeg&gravity=face"/>
+      <Img src={props.src} params="org_if_sml=0&force_format=webp,jpeg&func=cropfit&gravity=face" class="w-full h-full"/>
     </CloudimageProvider>
   );
 };
--- src/components/vue/CloudBackgroundImage.vue
+++ src/components/vue/CloudBackgroundImage.vue
@@ -2,8 +2,16 @@
 import BackgroundImg, { CloudImageProvider } from 'vue-cloudimage-responsive-plain';
 
 const cloudimageConfig = {
-	token: 'ajrctguoxo',
-	lazyLoading: true,
+  token: 'ajrctguoxo',
+  lazyLoading: true,
+  devicePixelRatioList: [1,2],
+  presets: {
+    xs: '(max-width: 639px)', 
+    sm: '(min-width: 640px)',
+    md: '(min-width: 768px)', 
+    lg: '(min-width: 1024px)', 
+    xl: '(min-width: 1280px)' 
+}
 };
 </script>
 
