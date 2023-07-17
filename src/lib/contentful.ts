ab6736c246ee0f584f350a372396a5997fd16c7f
Max Gilbert <mgilbert@aucklandmuseum.com>
2023-07-18 10:07:29 +1200

    preview

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -8,10 +8,8 @@ export const MaoriLocale = "mi-NZ";
 
 export const contentfulClient = contentful.createClient({
   space: import.meta.env.CONTENTFUL_SPACE_ID,
-  accessToken: import.meta.env.DEV
-    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
-    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
-  host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
+  accessToken: import.meta.env.CONTENTFUL_PREVIEW_TOKEN,
+  host: "preview.contentful.com"
 });
 
 export function getMenuGroupMenuItemsByName(
