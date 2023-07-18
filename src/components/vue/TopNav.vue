18dd94bd03536ba76720126f7433e1b4302d5f0f
Max Gilbert <mgilbert@aucklandmuseum.com>
2023-07-18 12:41:55 +1200

    Live Preview

--- src/components/vue/TopNav.vue
+++ src/components/vue/TopNav.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { ref } from "vue";
 import * as contentfulUtils from "lib/contentful.js";
 
 let navItems = await contentfulUtils.getMenuGroupMenuItemsByName("top", contentfulUtils.DefaultLocale);
@@ -11,7 +10,7 @@ let navItems = await contentfulUtils.getMenuGroupMenuItemsByName("top", contentf
 		<ul class="font-bold tracking-wide mt-0 flex w-auto divide-x-2 divide-gray-300 
 					[&>li]:self-center [&>li]:pl-4 [&>li]:pr-4 
 					[&>li:first-child]:pl-0 [&>li:last-child]:pr-0">
-			<li v-for="item in navItems" as="template">
+			<li v-for="item in navItems" as="template" >
 				<a class="text-center uppercase whitespace-nowrap" :title="item['fields']?.title.toString()"
 					:href="item['fields']?.url?.toString()">{{
 						item['fields']?.secondaryTitle ? item['fields']?.secondaryTitle : item['fields']?.title }}</a>
--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,6 +1,6 @@
-import { sequence } from "astro/middleware";
-import { i18nMiddleware } from "astro-i18n-aut";
+// import { sequence } from "astro/middleware";
+// import { i18nMiddleware } from "astro-i18n-aut";
 
-const i18n = i18nMiddleware({ defaultLocale: "en"});
+// const i18n = i18nMiddleware({ defaultLocale: "en"});
 
-export const onRequest = sequence(i18n);
+// export const onRequest = sequence(i18n);
